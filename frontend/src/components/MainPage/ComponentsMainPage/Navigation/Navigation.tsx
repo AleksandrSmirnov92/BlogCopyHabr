003fdfd95d1e580870cdd438292808f6f9d68c05
Aleksandr <ryan00@mail.ru>
2022-11-16 22:24:26 +0300

    Таблица сущностей

--- frontend/src/components/MainPage/ComponentsMainPage/Navigation/Navigation.tsx
+++ frontend/src/components/MainPage/ComponentsMainPage/Navigation/Navigation.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import NavigationCSS from "./Navigation.module.css";
 import signInIMG from "../../../../images/signIn.png";
 import allQuestionsIMG from "../../../../images/allQuestions.png";
-// import ExitIMG from "../../../../images/Exit.png";
-// import SettingsIMG from "../../../../images/Settings.png";
+
+import ExitIMG from "../../../../images/Exit.png";
+import SettingsIMG from "../../../../images/Settings.png";
 import allTagsIMG from "../../../../images/allTags.png";
 import usersIMG from "../../../../images/users.png";
-// import myProfile from "../../../../images/photoProfil.png";
+import myProfile from "../../../../images/photoProfil.png";
 import { NavLink } from "react-router-dom";
 const Navigation = () => {
   return (
     <nav className={NavigationCSS.navigation}>
       <ul>
-        {/* 
         <li className={NavigationCSS.myProfile}>
           <a href="#" className={NavigationCSS.photoProfil}>
             <img src={myProfile} className={NavigationCSS.photoProfil} />
@@ -38,13 +38,13 @@ const Navigation = () => {
           >
             Выход
           </NavLink>
-        </li> */}
-        <li className={NavigationCSS.signIn}>
+        </li>
+        {/* <li className={NavigationCSS.signIn}>
           <img src={signInIMG} alt="" />
           <NavLink to="/SignIn" className={NavigationCSS.signIn}>
             Войти на сайт
           </NavLink>
-        </li>
+        </li> */}
         <li className={NavigationCSS.allQuestions}>
           <img src={allQuestionsIMG} alt="" />
           <NavLink
