2b07ac596e196584ddee9d94daccc96b25fcec22
Aleksandr <ryan00@mail.ru>
2022-11-25 23:00:35 +0300

    не получается подключить базу данных на mac

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,16 +1,17 @@
 const Pool = require("pg").Pool;
-const pool = new Pool({
-  user: "postgres",
-  password: "skorpion19922111",
-  host: "localhost",
-  port: 5432,
-  database: "users",
-});
 // const pool = new Pool({
 //   user: "postgres",
-//   password: "",
+//   password: "skorpion19922111",
 //   host: "localhost",
 //   port: 5432,
 //   database: "users",
 // });
+const pool = new Pool({
+  user: "sura",
+  password: "1234",
+  host: "localhost",
+  port: 5432,
+  database: "users",
+});
+// pool.connect();
 module.exports = pool;
