c4ed78e781e8ae8b773c36106156ff80f24f4139
AleksandrSmirnov92 <ryan00@mail.ru>
2022-11-26 15:53:34 +0300

    Делаю авторизацию

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,17 +1,17 @@
 const Pool = require("pg").Pool;
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "skorpion19922111",
-//   host: "localhost",
-//   port: 5432,
-//   database: "users",
-// });
 const pool = new Pool({
-  user: "sura",
-  password: "",
+  user: "postgres",
+  password: "skorpion19922111",
   host: "localhost",
   port: 5432,
-  database: "sura",
+  database: "users",
 });
+// const pool = new Pool({
+//   user: "sura",
+//   password: "",
+//   host: "localhost",
+//   port: 5432,
+//   database: "sura",
+// });
 
 export default pool;
