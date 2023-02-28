83e3aa00b4e418eaae12cf63dd04439462b4ba79
AleksandrSmirnov92 <ryan00@mail.ru>
2023-02-28 15:55:05 +0300

    Делаю компонент тег

--- frontend/src/components/MainPage/ComponentsMainPage/Main/MainComponents/Tags/Tag/Tag.tsx
+++ frontend/src/components/MainPage/ComponentsMainPage/Main/MainComponents/Tags/Tag/Tag.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import TagCSS from "./Tag.module.css";
-import imgTag from "../../../../../../../images/JavascriptTag.png";
 import { useParams } from "react-router-dom";
 const Tag = () => {
   let { tagId } = useParams();
@@ -30,27 +29,26 @@ const Tag = () => {
     getInformationTag();
   }, []);
   return (
-    <div className={TagCSS.mainContainer}>
-      <header className={TagCSS.mainContainer__header}>
-        <img src={pathImg} alt="" className={TagCSS.img_tag} />
-        <h1 className={TagCSS.mainContainer__header__h1}>{nameTag}</h1>
-        <span className={TagCSS.mainContainer__header__count}>
-          {countFollowers}
-        </span>
-        <span className={TagCSS.mainContainer__header__followers}>
-          Подписчиков
-        </span>
+    <div className={TagCSS.tag_container}>
+      <header className={TagCSS.header}>
+        <img src={pathImg} alt="" />
+        <h1>{nameTag}</h1>
+        <span className={TagCSS.tag_count}>{countFollowers}</span>
+        <span className={TagCSS.tag_followers}>Подписчиков</span>
       </header>
       <main>
-        <nav className={TagCSS.main__navigation_panel}>
-          <a href="#" className={TagCSS.main__links}>
+        <nav className={TagCSS.nav}>
+          <a
+            href={`http://localhost:3000/tag/${tagId}`}
+            className={TagCSS.main__links}
+          >
             <span>Информация</span>
           </a>
-          <a href="#" className={TagCSS.main__links}>
+          {/* <a href="#" className={TagCSS.main__links}>
             <span>Ответы</span>
-          </a>
+          </a> */}
         </nav>
-        <p className={TagCSS.main__desciption}>{description}</p>
+        <p>{description}</p>
       </main>
     </div>
   );
