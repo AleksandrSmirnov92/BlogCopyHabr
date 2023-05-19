e96bfa55fb2f24ddd1c9a46ac7cbf009e6380831
AleksandrSmirnov92 <ryan00@mail.ru>
2023-05-19 12:41:40 +0300

    Делаю приложение

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -65,14 +65,11 @@ app.use("/getQuestionsId", getAllQuestionsId);
 app.use("/getAllInfo", getAllInfo);
 
 app.get("/*", function (req, res) {
-  res.sendFile(
-    path.join(__dirname, "../Frontend/build/index.html"),
-    function (err) {
-      if (err) {
-        res.status(500).send(err);
-      }
+  res.sendFile(path.join(__dirname, "../public/index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
     }
-  );
+  });
 });
 
 module.exports = app;
