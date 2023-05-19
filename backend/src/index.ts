b171ba40bbcc23a91a3c3f34431426ec3b39c15f
AleksandrSmirnov92 <ryan00@mail.ru>
2023-05-19 17:09:23 +0300

    исправляю баги

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -25,11 +25,9 @@ const path = require("path");
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-
-app.use(express.static(path.join(__dirname, "../public")));
-
 app.use(fileUpload());
-
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.static(path.join(__dirname, "../public/uploads")));
 app.use("/signIn", signInRouter);
 // ----------------------------------------
 app.use("/signUp", signUpRouter);
