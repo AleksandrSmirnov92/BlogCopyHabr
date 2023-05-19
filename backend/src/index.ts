be09d91a99e20f2a88d54506d403d7cdeaf35fb5
AleksandrSmirnov92 <ryan00@mail.ru>
2023-05-19 15:01:19 +0300

    Делаю сайт

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -26,8 +26,6 @@ app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
 
-// app.use(express.static(path.join(__dirname, "../Frontend/")));
-// app.use(express.static(path.join(__dirname, "../Frontend/build/")));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(fileUpload());
